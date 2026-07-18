a52113c359a535981802d41499f1bd337693cd5f
dwibagusd <115286631+dwibagusd@users.noreply.github.com>
2026-07-19 01:48:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ def predict_ordinal_probs_pymc(df_input, w, df_asli):
     nilai_z_bansos = (df_input[LEVEL2_VAR_KEY] - mean_z) / std_z
     gamma_z = nilai_z_bansos * w["gamma"]
 
-    u_prov = df_input[KOL_provinsi].map(w["u_Provinsi"]).fillna(0.0)
+    u_prov = df_input[KOL_provinsi].map(w["u_provinsi"]).fillna(0.0)
     phi_kab = df_input[KOL_kab_kota].map(w["phi_kabkota"]).fillna(0.0)
 
     eta = x_beta + gamma_z + u_prov + phi_kab
